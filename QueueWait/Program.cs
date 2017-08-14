e84f4cbf36b9f7f6bd99fa6b592e32ac0bad72f8
romolus <romolus@list.ru>
2017-08-14 21:02:28 +0300

    store

---
+++ QueueWait/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace QueueWait
+{
+    class Program
+    {
+
+        static QueueWait<int> _qw = new QueueWait<int>();
+
+        static void Main(string[] args)
+        {
+            var thPush1 = new Thread(SetPush1);
+            thPush1.Start();
+            var thPush2 = new Thread(SetPush2);
+            thPush2.Start();
+            var thPop = new Thread(GetPop);
+            thPop.Start();
+            Console.WriteLine("started");
+        }
+
+        static private void GetPop()
+        {
+            while (true)
+            {
+                var a = _qw.pop();
+                Console.WriteLine("получено значение "+a.ToString());
+            }
+        }
+
+        static private void SetPush1()
+        {
+            while (true)
+            {
+                Console.WriteLine("push10");
+                _qw.push(10);
+                Thread.Sleep(750);
+            }
+        }
+
+        static private void SetPush2()
+        {
+            while (true)
+            {
+                Console.WriteLine("push9");
+                _qw.push(9);
+                Thread.Sleep(500);
+            }
+        }
+
+    }
+}
---
+++ QueueWait/QueueWait.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace QueueWait
+{
+    /// <summary>
+    /// Очередь с ожиданием при получении
+    /// </summary>
+    public class QueueWait<T>
+    {
+        private object _locker = new object();
+        private Queue<T> _queue = new Queue<T>();
+
+        /// <summary>
+        /// push
+        /// </summary>
+        /// <param name="value"></param>
+        public void push(T value)
+        {
+            lock (_locker){
+                _queue.Enqueue(value);
+            }
+        }
+
+        /// <summary>
+        /// pop
+        /// </summary>
+        /// <returns></returns>
+        public T pop()
+        {
+            // если нету элементов то ждём
+            while (_queue.Count==0){
+                Thread.Sleep(20);
+            }
+            T ret;
+            lock (_locker){
+                ret = _queue.Dequeue();
+            }
+            return ret;
+        }
+
+    }
+}
